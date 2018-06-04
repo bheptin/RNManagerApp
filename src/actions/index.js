044b8374329de1ece0668bbc179f85f3dc408bae
Brian H <bheptin@gmail.com>
2018-06-04 16:59:12 -0500

    wire up creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './Types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './Types';
 import firebase from 'firebase';
 
 export const emailChanged = (text) => {
@@ -20,7 +20,7 @@ export const loginUser = ({ email, password }) => {
   return (dispatch) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(user => {
-        dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user });
+        dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
       });
   };
 };
