df12531488b17acf7ce0b27d3a161a8d357b8b55
Brian H <bheptin@gmail.com>
2018-06-06 13:23:59 -0500

    Add new Actions file and Reducer file.

---
+++ src/reducers/EmployeeFormReducer.js
@@ -0,0 +1,12 @@
+import {
+  EMPLOYEE_UPDATE
+} from './actions/Types';
+
+const INITIAL_STATE = {};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
