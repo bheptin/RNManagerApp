7984ed4f9698586ff7c7386a905886aefd76e77c
Brian H <bheptin@gmail.com>
2018-06-06 13:06:06 -0500

    Add new Scene for employee creation.

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
@@ -12,7 +13,15 @@ const RouterComponent = () => {
           </Scene>
 
           <Scene key="main">
-            <Scene key="employeeList" component={EmployeeList} title="List of Employees" />
+            <Scene
+              rightTitle="Add"
+              onRight={() => Actions.employeeCreate()}
+              key="employeeList"
+              component={EmployeeList}
+              title="List of Employees"
+              initial
+            />
+          <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
           </Scene>
 
       </Scene>
