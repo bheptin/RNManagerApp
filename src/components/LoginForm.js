5427a77ad33380f6d6a3f269ea10dd925e99458d
Brian H <bheptin@gmail.com>
2018-06-05 22:01:00 -0500

    Add INITIAL_STATE to AuthReducer to clear out email and password on success

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 
 class LoginForm extends Component {
 onEmailChange(text) {
@@ -16,6 +16,17 @@ onButtonPress() {
   const { email, password } = this.props;
   this.props.loginUser({ email, password });
 }
+
+renderButton() {
+  if (this.props.loading) {
+    return <Spinner size="large" />;
+  }
+  return (
+    <Button onPress={this.onButtonPress.bind(this)}>
+      Login
+    </Button>
+  );
+};
 
 renderError() {
   if (this.props.error) {
@@ -53,9 +64,7 @@ renderError() {
         {this.renderError()}
 
         <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>
-            Login
-          </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
@@ -70,9 +79,9 @@ const styles = {
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = ({auth}) => {
   const { email, password, error, loading } = auth;
-  
+
   return { email, password, error, loading };
 };
 
